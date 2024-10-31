c60f57804f956a60d41b0f6c679c485c7f4842a4
OscarHasselrot <o.hasselrot@gmail.com>
2024-10-31 17:44:19 +0100

    Cleanup

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main()
         {
-            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDatabas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
             var menu = new Menu();
 
             
